3c8282fdd68b8ca9cb5c8df08267f339a57d1f9e
Ayush singh <aayushssingh2003@gmail.com>
2025-10-16 22:00:11 +0530

    fetch student data

--- Desktop/FeeManagemnentsystem/src/functions/paymentstatus.js
+++ Desktop/FeeManagemnentsystem/src/functions/paymentstatus.js
@@ -1,13 +1,52 @@
 const { app } = require('@azure/functions');
+const sql = require("mssql");
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 app.http('paymentstatus', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+       const  studentid = request.query.studentid || request.body.studentid ;
+         if (!studentid) {
+        return context.res = { 
+        status: 404, 
+        body: { message: "Student ID required" } };
+        }
+        try {
+              const pool = await sql.connect(process.env.SQL_CONNECTION_STRING , 
+                console.log("Database connected")
+              );
+      const result = await pool.request()
+      .input('id', sql.Int, studentid)
+      .query(`SELECT Name, Course, TotalFee, PaidAmount, DueDate FROM Students WHERE StudentID = @id`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+      if(!result){
+         return context.res = { 
+        status: 404, 
+        body: { message: "Student not found" } };
     }
-});
+
+     const studentdata = result.recordsets[0]
+
+     if(!studentdata){
+        return context.res = {
+            status: 400,
+            body: {
+                message: "studentdata not found"
+            }
+        }
+     }
+console.log(studentdata);
+
+if (studentdata.)
+
+
+
+
+        } catch (error) {
+            
+        }
+
+    }})
